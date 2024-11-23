7b545126bbdb7e5b42a6cb5856a224dd5878dd6f
Terje Bruoygard <ivankishchenko@protonmail.com>
2024-11-23 02:54:37 -0300

    Update comments

--- TerjeRadiation/Scripts/4_World/Entities/ItemBase.c
+++ TerjeRadiation/Scripts/4_World/Entities/ItemBase.c
@@ -60,6 +60,14 @@ modded class ItemBase
 	{
 		super.ProcessVariables();
 		
+		/*
+		 This code block is private and was hidden before publishing on github.
+		 
+		 This repository does not provide full code of our mods need to be fully functional.
+		 That's just interfaces and simple logic that may be helpful to other developers while using our mods as dependencies.
+		 Modification, repackaging, distribution or any other use of the code from this file except as specified in the LICENSE.md is strictly prohibited.
+		 Copyright (c) TerjeMods. All rights reserved.
+		*/
 	}
 	
 	override void OnStoreSave(ParamsWriteContext ctx)
--- TerjeSkills/Scripts/4_World/Entities/PlayerBase.c
+++ TerjeSkills/Scripts/4_World/Entities/PlayerBase.c
@@ -116,6 +116,14 @@ modded class PlayerBase
     {
         bool result = super.CanBeTargetedByAI(ai);
 		
+		/*
+		 This code block is private and was hidden before publishing on github.
+		 
+		 This repository does not provide full code of our mods need to be fully functional.
+		 That's just interfaces and simple logic that may be helpful to other developers while using our mods as dependencies.
+		 Modification, repackaging, distribution or any other use of the code from this file except as specified in the LICENSE.md is strictly prohibited.
+		 Copyright (c) TerjeMods. All rights reserved.
+		*/
         
         return result;
     }
@@ -124,6 +132,14 @@ modded class PlayerBase
 	{
 		float result = super.GetWeightSpecialized(forceRecalc);	
 		
+		/*
+		 This code block is private and was hidden before publishing on github.
+		 
+		 This repository does not provide full code of our mods need to be fully functional.
+		 That's just interfaces and simple logic that may be helpful to other developers while using our mods as dependencies.
+		 Modification, repackaging, distribution or any other use of the code from this file except as specified in the LICENSE.md is strictly prohibited.
+		 Copyright (c) TerjeMods. All rights reserved.
+		*/
 		
 		return Math.Max(0.0, result);
 	}
